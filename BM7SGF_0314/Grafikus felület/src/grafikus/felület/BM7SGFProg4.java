6ba203285044c76f9775c6836e14c644e76523e1
kvnpapp <kvnpapp@gmail.com>
2024-03-14 15:39:41 +0100

    javítás

--- BM7SGF_0314/Grafikus felület/src/grafikus/felület/BM7SGFProg4.java
+++ BM7SGF_0314/Grafikus felület/src/grafikus/felület/BM7SGFProg4.java
@@ -7,11 +7,14 @@ public class BM7SGFProg4 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Grafikus felület");
         frame.setSize(400, 300);
+        frame.getContentPane().setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel("Helló BM7SGF!");
-        frame.add(label);
+        frame.getContentPane().add(label);
 
+        label.setBounds(40,100,100,20);
+        
         frame.setVisible(true);
     }
 }
