7be7d15803f660020f0c780981e90fd43e38b4e7
kvnpapp <kvnpapp@gmail.com>
2024-03-14 15:29:40 +0100

    Prog4

---
+++ BM7SGF_0314/Grafikus felület/src/grafikus/felület/BM7SGFProg4.java
@@ -0,0 +1,18 @@
+
+package grafikus.felület;
+
+import javax.swing.*;
+
+public class BM7SGFProg4 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("BM7SGF Program 4");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Helló BM7SGF!");
+        frame.add(label);
+
+        frame.setVisible(true);
+    }
+}
+
